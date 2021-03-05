af363d5409e2f4a26929fa92499a4e3932b1bf26
David Huarina <davidhuarina25@gmail.com>
2021-03-05 17:57:38 -0400

    actualizacion de datos reporte

--- assets/alerts/functionsGeneral.js
+++ assets/alerts/functionsGeneral.js
@@ -1,6 +1,18 @@
 function guardarPedidoDesdeFacturacion(){
 
-	var parametros={"codigo":codigo_s,"nombre":nombre_s,"ibnorca":cod_i,"fecha_curso":fecha_s};
+	var parametros={"tipoSalida":tipoSalida,"tipoDoc":tipoDoc,"cliente":cliente,
+	"tipoPrecio":tipoPrecio,"razonSocial":razonSocial,"nitCliente":nitCliente,"tipoVenta":tipoVenta,
+    "observaciones":observaciones,"totalVenta":totalVenta,"descuentoVenta":descuentoVenta,"totalFinal":totalFinal,
+    "efectivoRecibido":efectivoRecibido,"cambioEfectivo":cambioEfectivo,"fecha":fecha,"cantidad_material":cantidad_material};
+    
+    var index=0;var materiales=[];
+    for (var i=1; i<=cantidad_material; i++) {
+    	if($("#stock"+i).val()==0){
+    		index++;
+    		materiales["stock"+index]=$("#stock"+i).val();
+            //registrar detalle
+    	}
+    }
 	//PASA Y MOSTRAMOS LOS ESTADOS DE CUENTA    
     $.ajax({
         type: "POST",
