d0b8430ce4f6d1a2774245e87cef62dcb31e0a9b
David Huarina <davidhuarina25@gmail.com>
2021-03-06 13:15:07 -0400

    actualizacion datos cambio

--- sp/listadoAlmacenesTraspasosPendientes.php
+++ sp/listadoAlmacenesTraspasosPendientes.php
@@ -23,7 +23,7 @@ $fechaHasta="06/03/2021";
 $ipOrigen="10.10.1.11";
 $tabla_detalleOrigen="ADETALLE";
 $codCiudadOrigen=verificarAlmacenCiudadExistente("ALMACE"); //PONER EL $AGE1 DEL ALMACEN ORIGEN
-$age1Destino="A1";
+$age1Destino="A=";
 $codCiudadDestino=verificarAlmacenCiudadExistente($age1Destino);
 $codAlmacenDestino=obtenerCodigoAlmacenPorCiudad($codCiudadDestino);
 $sql="DELETE FROM ingreso_pendientes_almacenes where cod_almacen=$codAlmacenDestino";
@@ -38,7 +38,7 @@ foreach ($listAlma->lista as $alma) {
   $age1=$alma->age1;
   $cod_existe=verificarAlmacenCiudadExistente($age1);
   //QUERY SUCURSAL ORIGEN (ALMACEN)
-  $sql="SELECT am.DCTO,am.TIPO,am.GLO,am.FECHA,am.IDPROVEEDOR  FROM AMAESTRO am where am.STA!='B' AND am.TIPO='K' AND FECHA BETWEEN '$fechaDesde' AND '$fechaHasta' AND am.DAGE1='$age1'";
+  $sql="SELECT am.DCTO,am.TIPO,am.GLO,am.FECHA,am.IDPROVEEDOR,am.IDPER2  FROM AMAESTRO am where am.STA!='B' AND am.TIPO='K' AND FECHA BETWEEN '$fechaDesde' AND '$fechaHasta' AND am.DAGE1='$age1'";
   $dbh = new ConexionFarma(); 
   if($ipOrigen!="10.10.1.11"){// verificar si es almacen para no cambiar la ip por defecto
     $dbh->setHost($ipOrigen);
@@ -54,6 +54,7 @@ foreach ($listAlma->lista as $alma) {
       $gloOrigen=$row['GLO'];
       $fechaOrigen=$row['FECHA'];
       $codProveedor=$row['IDPROVEEDOR'];
+      $codPersonal=$row['IDPER2']; //revisar si guarda la columna con valores 0
       $codCiudadDestino=$cod_existe;
       $codigoUnico=obtenerCodigoTraspasoDocumentos($tabla_detalleOrigen,$tipoOrigen,$dctoOrigen,$ipOrigen);   
      ?>DOCUMENTO: <?=$dctoOrigen?><?php         
@@ -73,11 +74,11 @@ $respConf=mysqli_query($enlaceCon,$sqlConf);
 $banderaPrecioUpd=mysqli_result($respConf,0,0);
 
 
-$sql = "select IFNULL(MAX(cod_ingreso_almacen)+1,1) from ingreso_almacenes order by cod_ingreso_almacen desc";
+$sql = "select IFNULL(MAX(cod_ingreso_almacen)+1,1) from ingreso_pendientes_almacenes order by cod_ingreso_almacen desc";
 $resp = mysqli_query($enlaceCon,$sql);
 $codigo=mysqli_result($resp,0,0);
 
-$sql = "select IFNULL(MAX(nro_correlativo)+1,1) from ingreso_almacenes where cod_almacen='$global_almacen' order by cod_ingreso_almacen desc";
+$sql = "select IFNULL(MAX(nro_correlativo)+1,1) from ingreso_pendientes_almacenes where cod_almacen='$codAlmacenDestino' order by cod_ingreso_almacen desc";
 $resp = mysqli_query($enlaceCon,$sql);
 $nro_correlativo=mysqli_result($resp,0,0);
 
@@ -89,22 +90,14 @@ $nro_factura=0;// NRO DE FACTURA
 $observaciones=$gloOrigen;
 $proveedor=$codProveedor;
 
-$createdBy=$_COOKIE['global_usuario'];
+$createdBy=$codPersonal;
 $createdDate=date("Y-m-d H:i:s");
 
 $fecha_real=date("Y-m-d");
-
-
-               $consulta="insert into ingreso_almacenes (cod_ingreso_almacen,cod_almacen,cod_tipoingreso,fecha,hora_ingreso,observaciones,cod_salida_almacen,nota_entrega,nro_correlativo,ingreso_anulado,cod_tipo_compra,cod_orden_compra,nro_factura_proveedor,factura_proveedor,estado_liquidacion,cod_proveedor,created_by,modified_by,created_date,modified_date) values($codigo,$global_almacen,$tipo_ingreso,'$fecha_real','$hora_sistema','$observaciones','0','$nota_entrega','$nro_correlativo',0,0,0,$nro_factura,0,0,'$proveedor','$createdBy','0','$createdDate','')";
-
+               $consulta="insert into ingreso_pendientes_almacenes (cod_ingreso_almacen,cod_almacen,cod_tipoingreso,fecha,hora_ingreso,observaciones,cod_salida_almacen,nota_entrega,nro_correlativo,ingreso_anulado,cod_tipo_compra,cod_orden_compra,nro_factura_proveedor,factura_proveedor,estado_liquidacion,cod_proveedor,created_by,modified_by,created_date,modified_date) values($codigo,$global_almacen,$tipo_ingreso,'$fecha_real','$hora_sistema','$observaciones','0','$nota_entrega','$nro_correlativo',0,0,0,$nro_factura,0,0,'$proveedor','$createdBy','0','$createdDate','')";
+           echo $consulta."<br>";
                $sql_inserta = mysqli_query($enlaceCon,$consulta);
-
-
-       //insertar datos documento
-        $sqlInsert="INSERT INTO ingreso_pendientes_almacenes (cod_documento,tipo_documento,descripcion,fecha_salida,cod_ciudad_origen,cod_ciudad_destino,codigo_unico_generado) VALUES($dctoOrigen,'$tipoOrigen','$gloOrigen','$fechaOrigen',$codCiudadOrigen,$codCiudadDestino,'$codigoUnico')";
-        $executeInsert=mysqli_query($enlaceCon,$sqlInsert);
       }
-
 
   ?><br><?php       
     }   
