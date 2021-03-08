b3b177824ae40da5ec45fd2974f82ccb7d8d5511
David Huarina <davidhuarina25@gmail.com>
2021-03-07 21:41:51 -0400

    actualizacion datos ingreso por traspaso

--- sp/listadoAlmacenesTraspasosPendientes.php
+++ sp/listadoAlmacenesTraspasosPendientes.php
@@ -94,7 +94,7 @@ $createdBy=$codPersonal;
 $createdDate=date("Y-m-d H:i:s");
 
 $fecha_real=date("Y-m-d");
-               $consulta="insert into ingreso_pendientes_almacenes (cod_ingreso_almacen,cod_almacen,cod_tipoingreso,fecha,hora_ingreso,observaciones,cod_salida_almacen,nota_entrega,nro_correlativo,ingreso_anulado,cod_tipo_compra,cod_orden_compra,nro_factura_proveedor,factura_proveedor,estado_liquidacion,cod_proveedor,created_by,modified_by,created_date,modified_date) values($codigo,$global_almacen,$tipo_ingreso,'$fecha_real','$hora_sistema','$observaciones','0','$nota_entrega','$nro_correlativo',0,0,0,$nro_factura,0,0,'$proveedor','$createdBy','0','$createdDate','')";
+               $consulta="insert into ingreso_pendientes_almacenes (cod_ingreso_almacen,cod_almacen,cod_tipoingreso,fecha,hora_ingreso,observaciones,cod_salida_almacen,nota_entrega,nro_correlativo,ingreso_anulado,cod_tipo_compra,cod_orden_compra,nro_factura_proveedor,factura_proveedor,estado_ingreso,cod_proveedor,created_by,modified_by,created_date,modified_date) values($codigo,$global_almacen,$tipo_ingreso,'$fecha_real','$hora_sistema','$observaciones','0','$nota_entrega','$nro_correlativo',0,0,0,$nro_factura,0,0,'$proveedor','$createdBy','0','$createdDate','')";
            echo $consulta."<br>";
                $sql_inserta = mysqli_query($enlaceCon,$consulta);
       }
